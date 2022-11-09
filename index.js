09be58b725cbd8d4ccffe31de3e699ec25d9c6c1
Paul Thomas <paulthomas2107@gmail.com>
2022-11-09 16:46:46 +0000

    More....

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const fs = require('fs')
 const puppeteer = require('puppeteer')
 
 async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage()
+    await page.setDefaultNavigationTimeout(0);
     await page.goto('http://www.traversymedia.com')
+   
 
     //await page.screenshot({ path: "./images/example.png", fullPage: true })
     //await page.pdf({ path: "./images/example.pdf", format:'A4' })
@@ -17,8 +20,33 @@ async function run() {
     //const text = await page.evaluate(() => document.body.innerText)
     //console.log(text)
 
-    const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href))
-    console.log(links)
+    //const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href))
+    //console.log(links)
+
+    /*
+    const courses = await page.evaluate(() => Array.from(document.querySelectorAll('#courses .card'), (e) => ({
+        title: e.querySelector('.card-body h3').innerText,
+        level: e.querySelector('.card-body .level').innerText,  
+        url: e.querySelector('.card-footer a').href,
+        promo: e.querySelector('.card-footer .promo-code .promo').innerText,
+    })))
+    console.log(courses)
+    */
+   
+    const courses2 = await page.$$eval('#courses .card', (elements) => elements.map(e => ({
+        title: e.querySelector('.card-body h3').innerText,
+        level: e.querySelector('.card-body .level').innerText,  
+        url: e.querySelector('.card-footer a').href,
+        promo: e.querySelector('.card-footer .promo-code .promo').innerText,
+    })) )
+    console.log(courses2)
+
+    // Save output to JSON file
+    fs.writeFile('courses.json', JSON.stringify(courses2), (err) => {
+        if (err) throw err;
+        console.log('File Saved')
+    })
+
 
     await browser.close()
 }
